741acb7d0255d3e8973a6a415b32cc31c13bc4b4
gz-sans <juancamilotorresavila268@gmail.com>
2023-08-05 19:27:43 -0500

    gz-sans
    primera carga

---
+++ app/src/main/java/co/edu/sena/gzcompose/MainActivity.kt
@@ -0,0 +1,96 @@
+package co.edu.sena.gzcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.edu.sena.gzcompose.ui.theme.GzComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GzComposeTheme {
+                Conversation(SampleData.conversationSample)
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.huma_face1),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GzComposeTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
---
+++ app/src/main/java/co/edu/sena/gzcompose/SampleData.kt
@@ -0,0 +1,32 @@
+package co.edu.sena.gzcompose
+
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Camilo",
+            "¡Hola parcero! ¿Qué piensan hacer pa' la tarde? 😄"
+        ),
+        Message(
+            "Andrés",
+            "Estaba pensando en ir al parque a hacer un picnic 🧺🌳"
+        ),
+        Message(
+            "Duván",
+            "¡Suena chévere! Yo llevo unos snacks y bebidas z🥂🧺"
+        ),
+        Message(
+            "Camilo",
+            "¡Dale, parce! Yo llevo un frisbee y un balón de fútbol pa' jugar unos partidos ⚽🏐"
+        ),
+        Message(
+            "Andrés",
+            "¡Vacano! Nos encontramos en el parque a las 3 PM. ¡Nos vemos! ~_~"
+        ),
+        Message(
+            "Duván",
+            "¡Eso es! ¡A las 3 PM nos vemos! ^_^  "
+        )
+    )
+}
